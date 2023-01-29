0f99b13162b04e980c6803ddf2a28ac5880b3407
Joshua Caleb Bolito <bolitojoshuacaleb@gmail.com>
2023-01-30 06:21:54 +0800

    install mongoose

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,20 +2,20 @@ import React from "react";
 import navStyles from "../styles/Navigation.module.css";
 export default function Navigation() {
 	return (
-		<nav>
-			<div>
+		<nav className={navStyles.navbar}>
+			<div className={navStyles.logoAndSearch}>
 				<img src="/bare-logo.png" alt="Haribon" />
 				<label htmlFor="search" className={navStyles.search}>
 					<input type="text" name="search" id="search" />
 				</label>
 			</div>
-			<div>
+			<div className={navStyles.navigations}>
 				<img src="/icon-home.png" alt="Home" />
 				<img src="/icon-messages.png" alt="Messages" />
 				<img src="/icon-friends.png" alt="Friends" />
 				<img src="/icon-home.png" alt="Home" />
 			</div>
-			<div>Hi, Username!</div>
+			<div className={navStyles.userMenu}>Hi, Username!</div>
 		</nav>
 	);
 }
